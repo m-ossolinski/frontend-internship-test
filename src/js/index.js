47c90c5b4c565e69f8899ed23eab549b893ebfb6
Mateusz <mateusz.ossolinski@gmail.com>
2019-01-24 12:06:00 +0100

    task 1 - checked

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 /* Here goes your JS code */
+
+/* Reuseable DOM elements object*/
+const DOMelements = {
+    clickMe: '#show-popup-form',
+    loginModal: '.login-modal',
+    exitModal: '.exit-button'
+}
+
+/*Show login modal function and selector*/
+document.querySelector(DOMelements.clickMe).addEventListener('click', showModal);
+
+function showModal() {
+    document.querySelector(DOMelements.loginModal).style.display = 'flex';
+}
+
+
+/*Hide login modal function and selector*/
+document.querySelector(DOMelements.exitModal).addEventListener('click', hideModal);
+
+function hideModal() {
+    document.querySelector(DOMelements.loginModal).style.display = 'none';
+}
+
+
+
