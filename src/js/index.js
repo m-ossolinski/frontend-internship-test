150bc8ae4240899d8b452681b5a7c4087b9c59a7
Mateusz <mateusz.ossolinski@gmail.com>
2019-01-24 14:04:09 +0100

    task 2 - checked

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,13 @@
 const DOMelements = {
     clickMe: '#show-popup-form',
     loginModal: '.login-modal',
-    exitModal: '.exit-button'
+    exitModal: '.exit-button',
+    emailInput: '.email-input',
+    submitBtn: '.submit-form',
+    loginForm: '.login-form',
+    passwordForm: '.password-input',
+    checkbox: '.agree-checkbox',
+    showThankYou: '.show-ty'
 }
 
 /*Show login modal function and selector*/
@@ -22,5 +28,31 @@ function hideModal() {
     document.querySelector(DOMelements.loginModal).style.display = 'none';
 }
 
+
+/* Email validation, modal hiding and display 'Thank you'*/
+
+function formValidation() {
+    document.querySelector(DOMelements.submitBtn).addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const email = document.querySelector(DOMelements.emailInput).value;
+        const password = document.querySelector(DOMelements.passwordForm).value;
+        const checkbox = document.querySelector(DOMelements.checkbox);
+
+        const validator = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (email.match(validator) && password.length > 5 && checkbox.checked) {
+            console.log('Validation passed');
+        } else if(!email.match(validator)) {
+            alert('Your e-mail should look like jonn@doe.com');
+        } else if(password.length > 5 == false) {
+            alert('Your password should contain more than than 5 letters')
+        } else if(!checkbox.checked){
+            alert('Please accept terms & conditions');
+        }
+    });
+}
+
+formValidation();
 
 
